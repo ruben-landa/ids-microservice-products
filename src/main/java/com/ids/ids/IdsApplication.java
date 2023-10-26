4b5cd7d667357d68843ab24963b6c2546852c2de
Ruben Landa <landaitz104@gmail.com>
2023-10-26 12:00:50 -0600

    version is modified to java 8 spring and swagger

--- src/main/java/com/ids/ids/IdsApplication.java
+++ src/main/java/com/ids/ids/IdsApplication.java
@@ -2,22 +2,16 @@ package com.ids.ids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 
 
 @SpringBootApplication
-@EnableEurekaClient
 public class IdsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IdsApplication.class, args);
 	}
-
-	@Bean
+/*
+ * @Bean
 	public OpenAPI customOpenApi() {
 		return new OpenAPI()
 				.info(new Info()
@@ -25,5 +19,6 @@ public class IdsApplication {
 						.version("0.0.1")
 						.description("Microservice implementation of products"));
 
-	}
+	}*/
+	
 }
