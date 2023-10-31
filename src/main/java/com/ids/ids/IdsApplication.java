6d8adbb764ac199c8783ee522a5288fa5c488c1d
Ruben Landa <landaitz104@gmail.com>
2023-10-31 16:23:24 -0600

    version improvement and eureka server are added

--- src/main/java/com/ids/ids/IdsApplication.java
+++ src/main/java/com/ids/ids/IdsApplication.java
@@ -2,23 +2,14 @@ package com.ids.ids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class IdsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IdsApplication.class, args);
 	}
-/*
- * @Bean
-	public OpenAPI customOpenApi() {
-		return new OpenAPI()
-				.info(new Info()
-						.title("IDS ")
-						.version("0.0.1")
-						.description("Microservice implementation of products"));
 
-	}*/
-	
 }
